b52b1971870595a567b4d431474a4fe9d5c72d97
Shree Kottes <kottesh.j@gmail.com>
2025-10-21 23:39:41 +0530

    rewrite token creation utils

--- signer.py
+++ signer.py
@@ -1,12 +1,23 @@
 import hmac
-import hashlib
+from hashlib import sha256
 from config import REZConfig
 import time
 import base64
-from typing import Tuple
 import logging
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
+
+
+def base64_encode(b: bytes) -> str:
+    return base64.urlsafe_b64encode(b).decode().rstrip("=")
+
+
+def base64_decode(s: str) -> bytes:
+    missing = 4 - (len(s) % 4) % 4
+    s += "=" * missing
+
+    return base64.urlsafe_b64decode(s)
 
 
 def generate_token(data: str, expiry_in: int = 600) -> str:
@@ -14,34 +25,35 @@ def generate_token(data: str, expiry_in: int = 600) -> str:
 
     payload = f"{data}|{expiry}"
 
-    signature = hmac.new(
-        REZConfig.SECRET_KEY, payload.encode(), hashlib.sha256
-    ).hexdigest()
+    signature = hmac.new(REZConfig.SECRET_KEY, payload.encode(), sha256).digest()
 
-    return base64.urlsafe_b64encode(f"{payload}.{signature}".encode()).decode()
+    return f"{base64_encode(payload.encode())}.{base64_encode(signature)}"
 
 
-def verify_token(token: str) -> Tuple[str, bool]:
+def verify_token(token: str) -> tuple[str | None, bool]:
     try:
-        decoded = base64.urlsafe_b64decode(token.encode()).decode()
+        parts = token.split(".", 1)
 
-        payload, signature = decoded.rsplit(".", 1)
+        if len(parts) != 2:
+            logger.info(f"Invalid token | {token}") 
+            return None, False 
 
-        expected_sig = hmac.new(
-            REZConfig.SECRET_KEY, payload.encode(), hashlib.sha256
-        ).hexdigest()
+        payload, expected_sig = base64_decode(parts[0]), base64_decode(parts[1])
 
-        if not hmac.compare_digest(signature, expected_sig):
-            return "Invalid token", False
+        computed_sig = hmac.new(REZConfig.SECRET_KEY, payload, sha256).digest()
 
-        data, expiry = payload.rsplit("|", 1)
+        if not hmac.compare_digest(computed_sig, expected_sig):
+            logger.info(f"Token verification failed | {token} |Signature mismatch.")
+            return None, False
+
+        data, expiry = payload.decode().rsplit("|", 1)
 
         if int(time.time()) > int(expiry):
-            return "Token expired", False
+            logger.info(f"Token expired | {token} | Expired at: {datetime.fromtimestamp(expiry)}")
+            return None, False
 
         return data, True
 
     except Exception as e:
         logger.error(f"Failed to verify the token: {str(e)}")
-
-        return "Failed to verify the token", False
+        return None, False
